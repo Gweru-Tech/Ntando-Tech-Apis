b58edf220e67ea0f1a1d6f5e29375599eaf91e60
Gweru-Tech <Gwerutechnology@gmail.com>
2025-10-23 23:35:09 +0200

    Update youtube.js

--- src/api/search/youtube.js
+++ src/api/search/youtube.js
@@ -1,25 +1,45 @@
-module.exports = function(app) {
-    const yts = require('yt-search');
-    app.get('/search/youtube', async (req, res) => {
-        const { q } = req.query;
-        if (!q) {
-            return res.status(400).json({ status: false, error: 'Query is required' });
-        }
-        try {
-            const ytResults = await yts.search(q);
-            const ytTracks = ytResults.videos.map(video => ({
-                title: video.title,
-                channel: video.author.name,
-                duration: video.duration.timestamp,
-                imageUrl: video.thumbnail,
-                link: video.url
-            }));
-            res.status(200).json({
-                status: true,
-                result: ytTracks
-            });
-        } catch (error) {
-            res.status(500).json({ status: false, error: error.message });
-        }
+const axios = require('axios');
+const ytsr = require('ytsr');
+
+module.exports = async (req, res) => {
+  try {
+    const { q } = req.query;
+    
+    if (!q) {
+      return res.status(400).json({
+        status: false,
+        creator: "Ntando Mods",
+        message: "Please provide a search query"
+      });
+    }
+
+    const searchResults = await ytsr(q, { limit: 10 });
+    
+    const videos = searchResults.items
+      .filter(item => item.type === 'video')
+      .map(video => ({
+        title: video.title,
+        url: video.url,
+        thumbnail: video.bestThumbnail.url,
+        duration: video.duration,
+        views: video.views,
+        author: video.author.name
+      }));
+
+    res.json({
+      status: true,
+      creator: "Ntando Mods",
+      data: {
+        query: q,
+        results: videos
+      }
     });
-}
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      creator: "Ntando Mods",
+      message: "Error searching YouTube",
+      error: error.message
+    });
+  }
+};
